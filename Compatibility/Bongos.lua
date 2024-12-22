bc91c4ea670b015406120336612f2c3b959377c5
Mewtiny <bhhandley@gmail.com>
2024-12-21 18:54:54 -0600

    Updated Bongos compatibility to not update all buttons on each event.

--- Compatibility/Bongos.lua
+++ Compatibility/Bongos.lua
@@ -1,25 +1,8 @@
 CleveRoids.RegisterActionEventHandler(function(slot, event)
-    local MAX_BUTTONS = 120
+    if not slot or not BActionButton or not BActionBar then return end
 
-    if not BActionButton or not BActionBar then return end
-
-    for i = 1, 100 do
-        local bar = getglobal("BActionBar" .. i)
-        if not bar then break end
-
-        local start, stop = BActionBar.GetStart(i), BActionBar.GetEnd(i)
-
-        local offset = 0
-
-        local offset = BActionBar.GetPage(i)
-		if offset == 0 then offset = BActionBar.GetStance(i) end
-		if offset == 0 then offset = BActionBar.GetContext(i) end
-
-        if slot >= start + offset and slot <= stop + offset then
-            local button = getglobal("BActionButton" .. (slot - offset))
-            if button then
-                BActionButton.Update(button)
-            end
-        end
+    local button = getglobal("BActionButton" .. slot)
+    if button then
+        BActionButton.Update(button)
     end
 end)
